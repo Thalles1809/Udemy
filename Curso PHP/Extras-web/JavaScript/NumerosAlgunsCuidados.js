0d9af56fdb9405d9bc4d654996a16c40c93e79aa
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-04-28 00:52:17 -0300

    Create NumerosAlgunsCuidados.js

---
+++ Curso PHP/Extras-web/JavaScript/NumerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Infinity
+console.log("10" / 2) // Divide string, desde que tenha somente um número válido
+console.log("Show!" * 2) // Não calcula texto
+console.log(0.1 + 0.7) //Imprecisão no cálculo de decimais
+// console.log(10.ToString())
+console.log((10.345).toFixed(2))
