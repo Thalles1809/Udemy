19cf94ea543f4e238c31cb5dfc365b5f0bb12f97
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-24 20:27:47 -0300

    Create TipoBooleano.js

---
+++ Curso PHP/Extras-web/JavaScript/TipoBooleano.js
@@ -0,0 +1,29 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+isAtivo = 0
+console.log(!!isAtivo)
+console.log("\nOs verdadeiros...")
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!![])
+console.log(!!{})
+console.log(!!(isAtivo = true))
+
+console.log("\nOs falsos...")
+console.log(!!0)
+console.log(!!"")
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log("\nPara finalizar...")
+console.log(!!("" || null || 0 || " "))
