f75c9aa7b1d853739099261031ad2836c540dee3
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-24 23:21:08 -0300

    Update TipoObject.js

---
+++ Curso PHP/Extras-web/JavaScript/TipoObject.js
@@ -0,0 +1,17 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preço = 4998.90
+prod1['Desconto legal'] = 0.40 // Evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preço: 79.90,
+    obj: (property), blabla: number,
+        blabla: 1,
+        obj: {
+            blabla: 2
+        }
+
+}
