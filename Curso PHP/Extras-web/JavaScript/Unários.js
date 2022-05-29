d859448d50dd5907a2311b2d157b89957f659925
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-29 18:09:15 -0300

    Create Unários.js

---
+++ Curso PHP/Extras-web/JavaScript/Unários.js
@@ -0,0 +1,13 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+
+console.log(num1,num2)
+
+num1--
+
+console.log(num1,num2)
+
+console.log( ++num1 === num2-- ) // Evitar comparativo com incremento
+console.log(num1 === num2)
