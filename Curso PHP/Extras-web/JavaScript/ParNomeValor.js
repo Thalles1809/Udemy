8fc60a90be0168b13931003cbf40ad2f77c3c000
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-29 16:25:47 -0300

    Update ParNomeValor.js

--- Curso PHP/Extras-web/JavaScript/ParNomeValor.js
+++ Curso PHP/Extras-web/JavaScript/ParNomeValor.js
@@ -2,3 +2,23 @@
 
 const saudacao = 'Opa' // Contexto léxico 1
 
+function exec() {
+    const saudacao = 'Falaaa' // Contexto léxico 2
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    },
+    cidade: 'Boa Viagem'
+}
+
+console.log(saudacao)
+console.log(cliente)
+console.log(exec())
