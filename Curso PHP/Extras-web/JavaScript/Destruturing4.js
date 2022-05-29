06bf38c301f0d75440ce2c944939fbcb0e6feec7
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-29 17:14:45 -0300

    Update Destruturing4.js

--- Curso PHP/Extras-web/JavaScript/Destruturing4.js
+++ Curso PHP/Extras-web/JavaScript/Destruturing4.js
@@ -1,3 +1,12 @@
 function rand([min = 0, max = 1000]) {
-    
+    if (min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
 }
+
+console.log(rand([50,40])) // Inverte os valores min e max
+console.log(rand([30,40])) // Apresenta um valor aletário
+console.log(rand([990])) // Define um novo max
+console.log(rand([, 10])) // Define um novo min
+console.log(rand([])) // APresenta um valor aleatório entre os parametros da função
+console.log(rand()) // Erro undefined
