70a36e2268d3cf64d5f0d5baf4a0d83b1ab63733
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-29 16:06:57 -0300

    Update UsandoVarEmLoop.js

--- Curso PHP/Extras-web/JavaScript/UsandoVarEmLoop.js
+++ Curso PHP/Extras-web/JavaScript/UsandoVarEmLoop.js
@@ -1,5 +1,13 @@
+// For usando VAR
 for (var i = 0; i < 10; i++) {
     console.log(i)
 }
 
 console.log('i = ', i)
+
+// For usando LET
+for (let j = 0; j < 10; j++) {
+    console.log(j)
+}
+
+console.log('j = ', j)
