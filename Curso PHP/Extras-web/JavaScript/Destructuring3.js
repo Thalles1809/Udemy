00a04e37abb5fb6e57c26b939d697241b2f91ef9
Thalles Vinicius Franchin Peres Ladeira <thalles1809@gmail.com>
2022-05-29 17:06:50 -0300

    Update Destructuring3.js

--- Curso PHP/Extras-web/JavaScript/Destructuring3.js
+++ Curso PHP/Extras-web/JavaScript/Destructuring3.js
@@ -3,8 +3,8 @@ function rand({ min = 0, max = 1000}) {
     return Math.floor(valor)
 }
 
-const obj = {max: 50, min: 40}
+const obj = {max: 50, min: 40} // Define novo min e max
 console.log(rand(obj))
-console.log(rand({min: 955}))
-console.log(rand({max: 100}))
-console.log(rand({}))
+console.log(rand({min: 955})) // Define novo min
+console.log(rand({max: 100})) // Define novo max
+console.log(rand({})) // Numero aleatório com os valores padrões
